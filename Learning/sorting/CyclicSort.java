a62fbed7fdae779b58689ca66c242f2faeb6db94
demerwdani7452 <demerwdani@gmail.com>
2024-02-06 05:03:20 -0500

    update

---
+++ Learning/sorting/CyclicSort.java
@@ -0,0 +1,15 @@
+public class CyclicSort{
+	public static void main(String[] args){
+		int[] arr={3,5,2,1,4}
+
+}
+ static vid sort (int[] arr){
+	 int i=0;
+	 while(i<arr.length){
+		 int correct =arr[i]-1;
+		 if(arr[i]!=arr[correct]){
+			 swap(arr,i,correct);
+		 }
+		 else{
+			 i++;}}}}
+
